d62d5a597a31f50000289a36d675163ecae4b9b1
unknown <oficinadosdrones@gmail.com>
2022-02-25 10:38:21 +0000

    estudo de pipes

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PipeComponent } from './main/pipe/pipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PipeComponent
   ],
   imports: [
     BrowserModule,
