d3dd869e49857384a8c2d45b13b90bd3b20e11cc
unknown <oficinadosdrones@gmail.com>
2022-03-07 09:48:35 +0000

    criando componente modal

--- curso/src/app/_services/modal-directives/modal.directive.ts
+++ curso/src/app/_services/modal-directives/modal.directive.ts
@@ -1,33 +1,66 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+
 
 @Directive({
-  selector: '[appModal]'
+  selector: '[appModal]',
+  //   template: `
+  //   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  // <div class="modal-dialog">
+  // <div class="modal-content">
+  //   <div class="modal-header">
+  //     <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+
+  //   </div>
+  //   <div class="modal-body">
+  //     ...
+  //   </div>
+  //   <div class="modal-footer">
+  //     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+  //   </div>
+  // </div>
+  // </div>
+  // </div>
+  //   `
 })
 export class ModalDirective implements AfterViewInit {
+  @Input() localMessage: string = '';
+  @Input() localTitle: string = '';
+  @Input() showDisplayModal: string = '';
+ 
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {
+  console.log(this.localMessage, this.localTitle, this.showDisplayModal);
+  const modal2 = this.elementRef.nativeElement;
+  console.log('MOdal2',modal2)
+  
+  }
   ngAfterViewInit(): void {
-     
-      const modal = this.elementRef.nativeElement;
-      modal.div = `
-      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        
+    
+    const modal = this.elementRef.nativeElement;
+    modal.div = `
+    <div
+    class="modal"
+    tabindex="-1"
+    role="dialog"
+    [ngStyle]="{ display: ${this.showDisplayModal} }"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">${this.localTitle}</h4>
+        </div>
+        <div class="modal-body">
+          <p>${this.localMessage}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger">
+            Close
+          </button>
+        </div>
       </div>
     </div>
   </div>
-</div>
       `;
-      
   }
 }
--- curso/src/app/users/users/add-user/add-user.component.ts
+++ curso/src/app/users/users/add-user/add-user.component.ts
@@ -10,9 +10,12 @@ import { map, Subscription } from 'rxjs';
   styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements OnInit {
+  showDisplayModal: string = ''; // var do modalService
+  modalMessage:string = ''; // var do modalService
+  modalTitle:string = ''; // var do modalService
+  
   userFormBuilder!: FormGroup;
   hasError: boolean = false;
-  modalMessage: string = '';
   isSubmit: boolean = false;
   constructor(
     private formBuilder: FormBuilder,
@@ -20,34 +23,103 @@ export class AddUserComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    /**
+     * Obs: Caso o validators for mais de 1, tem que por em uma array
+     */
     this.userFormBuilder = this.formBuilder.group({
-      name: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(100)]],
-      username: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      phone: [null, [Validators.required, Validators.minLength(9), Validators.maxLength(12)]],
+      name: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(100),
+        ],
+      ],
+      username: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(10),
+        ],
+      ],
+      phone: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(9),
+          Validators.maxLength(12),
+        ],
+      ],
       email: [null, [Validators.required, Validators.email]],
       address: this.formBuilder.group({
-        street: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        suite: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]],
-        city: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
-        zipecode: [null, [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
+        street: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30),
+          ],
+        ],
+        suite: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(10),
+          ],
+        ],
+        city: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(30),
+          ],
+        ],
+        zipecode: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(7),
+            Validators.maxLength(7),
+          ],
+        ],
       }),
       company: this.formBuilder.group({
-        name: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
-        catchphrase: [null,[Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
-        bs: [null,[Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
+        name: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(20),
+          ],
+        ],
+        catchphrase: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(20),
+          ],
+        ],
+        bs: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(20),
+          ],
+        ],
       }),
     });
     /**
      * para pegarmos os erros podemos usar o CONTROLS ou o GET, no caso do get passamos nó do objeto.
      */
     // console.log(`uCONTROLS company`, this.userFormBuilder.controls['company']);
-    // console.log(`uCONTROLS company`, this.userFormBuilder.get('company.bs'))
-    
+    // console.log(`get company`, this.userFormBuilder.get('company.bs'))
   }
-  
-  /**
-   * Obs: Caso o validators for mais de 1, tem que por em uma array
-   */
+
   sendUserForm() {
     this.userService.postUser(this.userFormBuilder.value).subscribe({
       next: (result) => {
@@ -55,7 +127,9 @@ export class AddUserComponent implements OnInit {
           this.hasError = false;
           this.isSubmit = true;
           this.modalMessage = 'Formulario Submetido com Sucess';
-          this.resetForm();
+          this.modalTitle = 'Success';
+          this.openPopup();
+          // this.resetForm();
         }
       },
       error: (err) => {
@@ -66,7 +140,14 @@ export class AddUserComponent implements OnInit {
       complete: () => {},
     });
   }
-  resetForm(){
+  resetForm() {
     this.userFormBuilder.reset();
+  }
+
+  openPopup() {
+    this.showDisplayModal = 'block';
+  }
+  closePopup() {
+    this.showDisplayModal = 'none';
   }
 }
